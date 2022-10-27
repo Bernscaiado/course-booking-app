4994d09266987d757309a02cfd3f8ec4210d1ce7
Bernscaiado <becaiado@gmail.com>
2022-10-27 18:14:08 -0400

    added main homescreen

---
+++ app/src/main/java/com/example/coursebooking/Admin.java
@@ -0,0 +1,9 @@
+package com.example.coursebooking;
+
+public class Admin extends User{
+
+    public Admin(String username, String firstname, String role) {
+        super(username, firstname, role);
+    }
+
+}
