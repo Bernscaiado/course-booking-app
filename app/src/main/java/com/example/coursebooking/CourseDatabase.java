65d5d67bcab8fb35967ee1c74c6bdb68e3845131
Bernscaiado <becaiado@gmail.com>
2022-11-14 12:35:39 -0500

    fully functional assign/unassign feature

--- app/src/main/java/com/example/coursebooking/CourseDatabase.java
+++ app/src/main/java/com/example/coursebooking/CourseDatabase.java
@@ -81,14 +81,14 @@ public class CourseDatabase extends SQLiteOpenHelper {
     public boolean nullInstructor(String name, String code, String instructor) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(COLUMN_INSTRUCTOR, "NULL");
+        cv.putNull(COLUMN_INSTRUCTOR);
         db.update(TABLE_COURSES,cv,"name = ? and code = ? AND instructor = ?",new String[]{name,code,instructor});
         return true;
     }
 
     public boolean hasInstructor(String coursename, String coursecode) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM courses WHERE name = ? AND code = ? AND instructor != NULL", new String[] {coursename,coursecode});
+        Cursor cursor = db.rawQuery("SELECT * FROM courses WHERE name = ? AND code = ? AND instructor IS NOT NULL", new String[] {coursename,coursecode});
         return cursor.getCount()>0;
     }
 
