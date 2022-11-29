9751b6c21c1c67b92b70fd5e65a4bb1d056e030f
Bernscaiado <becaiado@gmail.com>
2022-11-28 20:52:59 -0500

    added frontend of student page

--- app/src/main/java/com/example/coursebooking/MainActivity4.java
+++ app/src/main/java/com/example/coursebooking/MainActivity4.java
@@ -2,7 +2,11 @@ package com.example.coursebooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -10,5 +14,56 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        Button list = findViewById(R.id.listCourses);
+        Button enroll = findViewById(R.id.enroll);
+        Button searchCourse = findViewById(R.id.searchCourse);
+
+
+        Intent intent = getIntent();
+        String str1 = intent.getStringExtra("firstname");
+        String str2 = intent.getStringExtra("username");
+        String str3 = intent.getStringExtra("role");
+
+
+        TextView receiver_msg =  findViewById(R.id.received_value_id2);
+        receiver_msg.setText(str1 + "/" + str2 + "! You are logged in as '" + str3 + "'");
+
+        list.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ListCoursesActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        }));
+
+        searchCourse.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SearchCourseActivity.class);
+                intent.putExtra("firstname", str1);
+                intent.putExtra("username", str2);
+                intent.putExtra("role", str3);
+
+                v.getContext().startActivity(intent);
+            }
+        }));
+
+        enroll.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), CourseEnrollActivity.class);
+
+                intent.putExtra("firstname", str1);
+                intent.putExtra("username", str2);
+                intent.putExtra("role", str3);
+
+                v.getContext().startActivity(intent);
+            }
+        }));
+
+
+
+
     }
 }
