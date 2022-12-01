cf5c71d7dab46ba070098f6aad92eb3e7de46f25
bruno <kbrunojr55@gmail.com>
2022-11-30 21:29:56 -0500

    adding 5 more unit test to this deliverable and Student search course by: name,code,day

---
+++ app/src/test/java/com/example/coursebooking/CourseDatabaseTest.java
@@ -0,0 +1,42 @@
+package com.example.coursebooking;
+
+
+import static org.junit.Assert.*;
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class CourseDatabaseTest {
+    Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    //SearchCourseActivity searchCourseActivity = new SearchCourseActivity();
+    private Course course;
+    private CourseDatabase db = new CourseDatabase(appContext);
+
+    @Test
+    public void getDataTest() {
+        int listNum = db.getData().getCount();
+        assertEquals(1,listNum);
+    }
+
+//    @org.junit.jupiter.api.Test
+//    void testGetData() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void findByName() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void findByCode() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void findByDay() {
+//    }
+}
